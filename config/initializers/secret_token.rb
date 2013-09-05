c42c5a7b80083a5553f932fe6de79c74ecff07ad
Tom Kennedy <tom@refactory.co>
2013-09-05 15:40:08 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Refactory::Application.config.secret_key_base = '49aaab9a0de7fbf6906c462a576c4ba3d7fe33ffed2cb430ecfd033e536c4505db6140dba1a82c916b61c859e09b1f0183acc7c378a669488d7513774ddba7fc'
